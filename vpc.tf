c793ec338fecb5e48a6a81d7e8c8e6499bf5dca1
mouli90525 <chandramouliyedla123@gmail.com>
2023-12-20 03:22:27 +0000

    create pvt subnet A & B

--- vpc.tf
+++ vpc.tf
@@ -29,3 +29,25 @@ resource "aws_subnet" "tf-ecomm-pub-sn-B" {
   }
 }
 
+
+# create pvt sub AZ - A
+resource "aws_subnet" "tf-ecomm-pvt-sn-A" {
+  vpc_id     = aws_vpc.tf-ecomm.id
+  cidr_block = "192.168.2.0/24"
+   availability_zone ="us-east-2a"
+   map_public_ip_on_launch = "false"
+   tags = {
+    Name = "ecomm-private-subnet-A"
+  }
+}
+# create pvt sub AZ - B
+resource "aws_subnet" "tf-ecomm-pvt-sn-B" {
+  vpc_id     = aws_vpc.tf-ecomm.id
+  cidr_block = "192.168.3.0/24"
+   availability_zone ="us-east-2b"
+   map_public_ip_on_launch = "false"
+   tags = {
+    Name = "ecomm-private-subnet-B"
+  }
+}
+
