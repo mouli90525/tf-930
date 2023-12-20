d404adf91aac4f86779956ac0d362442a2c00cac
mouli90525 <chandramouliyedla123@gmail.com>
2023-12-20 03:09:32 +0000

    create public subnet -B

--- vpc.tf
+++ vpc.tf
@@ -6,6 +6,7 @@ resource "aws_vpc" "tf-ecomm" {
     Name = "ecomm-vpc"
   }
 }
+# create pub sub AZ - A
 
 resource "aws_subnet" "tf-ecomm-pub-sn-A" {
   vpc_id     = aws_vpc.tf-ecomm.id
@@ -14,6 +15,17 @@ resource "aws_subnet" "tf-ecomm-pub-sn-A" {
    map_public_ip_on_launch = "true" 
    tags = {
     Name = "ecomm-public-subnet-A"
+  }
+}
+
+# create pub sub AZ - B
+resource "aws_subnet" "tf-ecomm-pub-sn-B" {
+  vpc_id     = aws_vpc.tf-ecomm.id
+  cidr_block = "192.168.1.0/24"
+   availability_zone ="us-east-2b"
+   map_public_ip_on_launch = "true"
+   tags = {
+    Name = "ecomm-public-subnet-B"
   }
 }
 
